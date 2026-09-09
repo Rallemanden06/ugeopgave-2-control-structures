a533d914f99f49cbdcd49da79536562f9408461c
Rallemanden06 <rnl06@outlook.dk>
2026-09-09 14:10:40 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,171 @@
 public class Main {
+    public static void main(String[] args) {
+
+        // Opgave1
+        /*
+        double totalPrice = 1200.0;
+        double finalPrice = 0;
+
+        if (totalPrice > 1000){
+            finalPrice = totalPrice * 0.8;
+            System.out.println("Discount applied!");
+        }
+        System.out.println("Final price: " + finalPrice + "kr");
+        */
+        // Opgave2
+        /*
+        double weight = 62;
+        double height = 172;
+
+        double BMI = weight / (height*height);
+
+        if (BMI >= 25){
+            System.out.println("overwight");
+        } else if (BMI >= 18.5){
+            System.out.println("Normal overwight");
+        } else {
+            System.out.println("Underweight");
+        }
+        */
+        // Opgave3
+        /*
+        int hour = 14;
+
+        if (hour >= 5 && hour <= 11) {
+            System.out.println("Good morning!");
+        } else if (hour >= 12 && hour <= 17) {
+            System.out.println("Good afternoon!");
+        } else if (hour >= 18 && hour <= 21) {
+            System.out.println("Good evening!");
+        } else {
+            System.out.println("Good night!");
+        }
+        */
+        // Opgave4
+        /*
+        int score = 88;
+        char grade;
+        String status = " ";
+        String comment = " ";
+
+        if (score >= 90) {
+            grade = 'A';
+            comment = "Exellent";
+        } else if (score >= 80) {
+            grade = 'B';
+            comment = "Good";
+        } else if (score >= 70) {
+            grade = 'C';
+            comment = "Satisfactory";
+        } else if (score >= 70) {
+            grade = 'D';
+            comment = "Needs improvement";
+        } else {
+            grade = 'F';
+            comment = "Failing";
+        }
+
+        if (score >= 60){status= "PASS";}
+        else {status = "Fail";}
+
+        System.out.println("=== STUDENT REPORT===");
+        System.out.println("Score: " + score);
+        System.out.println("Grade: " + grade);
+        System.out.println("Status: " + status);
+        System.out.println("Comment: " + comment);
+        */
+        // Opgave5
+        /*
+        double totalPrice = 350;
+        boolean isMember = true;
+        double shippingFee;
+
+        if (totalPrice > 500 || (isMember && totalPrice > 200)){
+            System.out.println("Free shipping");
+            shippingFee = 0.0;
+        } else {
+            System.out.println("Shipping fee");
+             shippingFee = 50.0;
+        }
+
+        double finalPrice = totalPrice + shippingFee;
+
+        System.out.println("Total price: " + finalPrice);
+        */
+        // Opgave6
+        /*
+        int guestAge = 14;
+        int ageLimit = 15;
+        boolean hasParentalConsent = true;
+
+        if (guestAge >= ageLimit || (guestAge >= 13 && hasParentalConsent)){
+            System.out.println("Can watch movie");
+        } else {
+            System.out.println("Cant watch movie");
+        }
+        */
+        // Opgave7
+        /*
+        int partySize = 4;
+        int available = 6;
+        boolean hasReservation = false;
+        int waitTime = 20;
+        boolean restaurantFull = false;
+
+        if (partySize <= available && (hasReservation || waitTime < 30) && restaurantFull){
+            System.out.println("Table available");
+            System.out.println("Estimated wait: " + waitTime + " minutes");
+        } else {
+            System.out.println("No table available");
+        }
+        */
+        // Opgave8
+
+
+
+        // Opgave9
+
+        // Opgave10
+
+        // Opgave11
+
+        // Opgave12
+
+        // Opgave13
+
+        // Opgave14
+
+        // Opgave15
+
+        // Opgave16
+
+        // Opgave17
+
+        // Opgave18
+
+        // Opgave19
+
+        // Opgave20
+
+        // Opgave21
+
+        // Opgave22
+
+        // Opgave23
+
+        // Opgave24
+
+        // Opgave25
+
+        // Opgave26
+
+        // Opgave27
+
+        // Opgave28
+
+        // Opgave29
+
+
+    }
 }
+
